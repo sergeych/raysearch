9c9304dfe90be99d3b9015eabf02559e3b4480b6
sergeych <real.sergeych@gmail.com>
2023-09-08 03:06:35 +0100

    fix #15 background mode with tray icon, with workaround of the java trayicon bug in linux

---
+++ NOTES.md
@@ -0,0 +1,10 @@
+# Thanks!
+
+This work uses many free and open sources modules, some direct dependencies and my big thanks to:
+
+- Feather Icons -- add link
+
+- ??? [Search icon](https://icons8.com/icon/132/search">Search) icon by [Icon8](https://icons8.com)
+
+- [Apache Lucene](https://lucene.apache.org/)
+- Many file format packages I'm still working with (TBD)
--- src/main/kotlin/EightRaysSearch.kt
+++ src/main/kotlin/EightRaysSearch.kt
@@ -1,10 +1,16 @@
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.input.key.isCtrlPressed
 import androidx.compose.ui.input.key.key
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.window.Tray
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberTrayState
 import net.sergeych.Config
 import net.sergeych.appHomePath
 import net.sergeych.mp_logger.FileLogCatcher
@@ -15,23 +21,78 @@ import kotlin.io.path.pathString
 
 //private val log = LogTag("MAIN")
 
-@OptIn(ExperimentalComposeUiApi::class)
-fun main() = application {
-    Log.connectConsole(Log.Level.INFO)
-    FileLogCatcher(appHomePath.resolve("raysearch.log").pathString, Config.fileLogLevel,true)
-    Log.defaultLevel = Log.Level.DEBUG
+enum class PlatformType {
+    Linux, Windows, Macos, Unknown;
 
-    Scanner.setup(listOf(System.getProperties().getProperty("user.home")))
+    val isLinux: Boolean get() = this == Linux
+}
 
-    Window(
-        onCloseRequest = ::exitApplication, title = "8 Rays search", onKeyEvent = {
-            if (it.isCtrlPressed && it.key == Key.Q)
-                System.exit(0)
-            false
-        },
-        icon = painterResource("raysearch.svg")
-    ) {
-        App()
+val detectedPlatform: PlatformType by lazy {
+    when (System.getProperty("os.name").lowercase()) {
+        "linux" -> PlatformType.Linux
+        "windows" -> PlatformType.Windows
+        // I'm not sure about it as there is no reason to run it on mac
+        "macos" -> PlatformType.Macos
+        else -> PlatformType.Unknown
     }
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
+fun main() {
+    application {
+        var isOpen by remember { mutableStateOf(true) }
+
+        Log.connectConsole(Log.Level.INFO)
+        FileLogCatcher(appHomePath.resolve("raysearch.log").pathString, Config.fileLogLevel, true)
+        Log.defaultLevel = Log.Level.DEBUG
+
+
+        val trayState = rememberTrayState()
+
+        Tray(
+            state = trayState,
+            icon = painterResource(
+                if (detectedPlatform.isLinux)
+                    "linux_tray_icon.png"
+                else
+                    "tray_icon.svg"
+            ),
+            onAction = {
+                       isOpen = true
+            },
+            menu = {
+                Item("Open",
+                    onClick = {
+                        isOpen = true
+                    })
+                Separator()
+                Item(
+                    "Exit raysearch",
+                    onClick = {
+                        exitApplication()
+                    }
+                )
+            }
+        )
+        // we don't want to start it if it failed tray initialization
+
+        Scanner.setup(listOf(System.getProperties().getProperty("user.home")))
+
+        if (isOpen) {
+            Window(
+                onCloseRequest = {
+                    isOpen = false
+                },
+                title = "8 Rays search",
+                onKeyEvent = {
+                    if (it.isCtrlPressed && it.key == Key.Q)
+                        isOpen = false
+                    false
+                },
+                icon = painterResource("raysearch.svg")
+            ) {
+                App()
+            }
+        }
+    }
+}
